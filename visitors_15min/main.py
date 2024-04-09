388fae521150e45161eea43f6debd7b24f6f2638
Steve Rosam <srosam+quix-demos@gmail.com>
2024-04-09 14:04:49 +0000

    Edited 'main.py'

--- visitors_15min/main.py
+++ visitors_15min/main.py
@@ -35,7 +35,7 @@ def main():
     # create a 15 minute hopping window with a 10 second step
     # pass each row to the row_processor function
     # take the final result of the window
-    sdf = sdf.hopping_window(timedelta(minutes=15), timedelta(seconds=10)).reduce(row_processor, lambda r: row_processor({}, r)).final()
+    sdf = sdf.hopping_window(timedelta(minutes=15), timedelta(seconds=10)).reduce(row_processor, lambda r: row_processor({}, r)).current()
     # data produced by hopping window is: {'start': 1710861638000, 'end': 1710862538000, 'value': {'ABC123': 1}}
 
     # now we overwrite the 'value' column with the count of 
